49a17f99966d30f966ee57f752b2118f6733b8bb
Zemlaman <matej.zemla@seznam.cz>
2020-12-01 10:17:55 +0100

    Jupí úkol

--- ManagerWEB/src/main/java/cz/educanet/webik/UserResource.java
+++ ManagerWEB/src/main/java/cz/educanet/webik/UserResource.java
@@ -1,5 +1,11 @@
 package cz.educanet.webik;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("users")
+@Produces(MediaType.APPLICATION_JSON
 public class UserResource {
 
 }
