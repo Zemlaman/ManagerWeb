37c6bc5be20e45575c26a7e4f8782ad6d6d6c5fe
Zemlaman <matej.zemla@seznam.cz>
2020-12-07 18:03:59 +0100

    Progressssss

--- ManagerWEB/src/main/java/cz/educanet/webik/User.java
+++ ManagerWEB/src/main/java/cz/educanet/webik/User.java
@@ -1,14 +1,34 @@
 package cz.educanet.webik;
 
 public class User {
+    private String FirstName;
+    private String LastName;
     private String username;
+    private String email;
     private String password;
-
-    public User(String username, String password) {
+    public User(String FirstName, String LastName, String username, String email, String password) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
         this.username = username;
+        this.email = email;
         this.password = password;
     }
 
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
     public String getUsername() {
         return username;
     }
@@ -17,15 +37,19 @@ public class User {
         this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void renameUser(String newUsername) {
-        this.username = newUsername;
     }
 }
--- ManagerWEB/src/main/java/cz/educanet/webik/UserResource.java
+++ ManagerWEB/src/main/java/cz/educanet/webik/UserResource.java
@@ -1,11 +1,61 @@
 package cz.educanet.webik;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Path("users")
-@Produces(MediaType.APPLICATION_JSON
-public class UserResource {
+@Produces(MediaType.APPLICATION_JSON)
+public class UsersResource {
+    private static List<User> names = new ArrayList<User>();
+
+    @GET
+    public Response getAll() {
+        return Response.ok(names).build();
+    }
+
+    @POST
+    public Response createUser(
+            @QueryParam("FirstName") String FirstName, @QueryParam("LastName") String LastName,
+            @QueryParam("username") String username, @QueryParam("email") String email,
+            @QueryParam("password") String password
+    ) {
+        User user = new User(FirstName, LastName, username, email, password);
+        if(userCheck(user)) {
+            return Response.ok("This username already exists").build();
+        }
+        names.add(user);
+        return Response.ok("This username is ok").build();
+    }
+
+    @POST
+    public Response loginUser(){
+
+    }
+
+    public Boolean userCheck(User user) {
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).getUsername().equals(user.getUsername())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @DELETE
+    public Response removeUser(User user) {
+        if(userCheck(user)) {
+            names.remove(user);
+            return Response.ok().build();
+        } else {
+            return Response.serverError().build();
+        }
+    }
+
 
 }
